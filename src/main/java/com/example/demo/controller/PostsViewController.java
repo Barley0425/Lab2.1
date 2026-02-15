c42f50e203d9628a2843ff299b2e77eeae403866
Barley0425 <russkiy127@gmail.com>
2026-02-15 20:54:49 +0500

    Add Thymeleaf template engine and list.html view

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
 
     @ResponseBody
